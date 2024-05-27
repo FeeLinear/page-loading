2dbe217f9ade099f41f5a0f2754d88ac64cbdf73
FeeLinear <444730413@qq.com>
2024-05-27 14:10:49 +0800

    update:插件完成！

--- page-loading.js
+++ page-loading.js
@@ -1,12 +1,12 @@
 /*
  *
  * @Name: pageLoading
- * @Description: 页面加载水波loading进度插件
+ * @Description: 页面加载loading进度插件
  * @Version: v1.0.0
  * @Date: 2024-05-23 09:34:53
  * @Author: FeeLinear 444730413@qq.com
  * @LastEditors: FeeLinear 444730413@qq.com
- * @LastEditTime: 2024-05-27 09:37:47
+ * @LastEditTime: 2024-05-27 14:10:08
  */
 ;(function () {
   
@@ -28,52 +28,60 @@
       })
     }
     this.options = Object.assign(this.defaultOptions, options);
-    if(!this.options.bgColor){
+    if(this.options.bgColor === undefined){
       this.options.bgColor = this.options.theme === "dark" ? "#060000" : "#cfecfe";
     }
-    if(!this.options.bgUrl){
+    if(this.options.bgUrl === undefined){
       this.options.bgUrl = `https://feelinear.github.io/page-loading/images/loading-bg-${this.options.theme}.jpg`;
     }
-    if(this.options.logo.show && !this.options.logo.url){
+    if(this.options.logo.url  === undefined){
       this.options.logo.url = "https://feelinear.github.io/page-loading/images/logo.png";
     }
-    if(!this.options.wave.url){
-      this.options.wave.url = "https://feelinear.github.io/page-loading/images/wave-mixin.png";
+    if(this.options.animation.url  === undefined){
+      this.options.animation.url = `https://feelinear.github.io/page-loading/images/loading-${this.options.animation.type}.png`;
     }
-    if(!this.options.container.url){
+    if(this.options.container.url  === undefined){
       this.options.container.url = `https://feelinear.github.io/page-loading/images/loading-ball-${this.options.theme}.png`;
     }
-    this.options.wave.height = parseInt(this.options.container.height) * 6 / 5;
+    this.options.animation.height = parseInt(this.options.container.height) * 6 / 5;
     this.init();
   }
   PageLoading.prototype = {
     defaultOptions: {
-      theme: "light", // dark
-      bgUrl: "",
-      bgColor: "",
+      theme: "light", // dark 
+      bgUrl: undefined,
+      bgColor: undefined,
       logo: {
-        show: true,
-        url: "",
+        url: undefined,
         width: "100px",
         height: "100px",
         opacity: 1,
       },
-      wave: {
-        url: "",
+      animation: {
+        type: "wave", // ring
+        url: undefined,
         opacity: 1,
         speed: 50,
         flatness: 4,
       },
       container: {
-        url: "",
-        width: "250px",
-        height: "250px",
-        top: "50%",
+        url: undefined,
+        width: "256px",
+        height: "256px",
+        top: "45%",
         left: "50%"
       },
       process: {
-        color: "#fff",
-        fontSize: "48px",
+        type: "bar", // number
+        width: "588px",
+        height: "14px",
+        left: "50%",
+        bottom: "25%",
+        color: "#0066FF",
+        bgColor: "#CCD8E9",
+        activeColor1: "#88DBFF",
+        activeColor2: "#0066FF",
+        fontSize: "20px",
         labelFontSize: "16px",
       }
     },
@@ -108,6 +116,14 @@
         overflow: hidden;
         z-index: 2;
       }
+      .ring-box{
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background: url(./images/loading-ring.png) no-repeat center;
+        background-size: 100% 100%;
+        animation: rotate 2s linear infinite;
+      }
       .ball-center{
         position: absolute;
         width: 100%;
@@ -116,10 +132,12 @@
         top: 0;
         left:0;
         opacity: ${this.options.logo.opacity};
-        color: ${this.options.process.color};
         ${this.options.logo.url ? "background: url(" + this.options.logo.url + ") no-repeat center;":""}
         background-size: ${this.options.logo.width} ${this.options.logo.height};
         z-index: 3;
+      }
+      .bar-color{
+        color: ${this.options.process.color};
       }
       .process-box{
         display: ${this.options.logo.url?"none":"block"};
@@ -134,11 +152,11 @@
       }
       .wave-container{
         position: relative;
-        width: ${this.options.wave.flatness * 500}px;
-        height: ${this.options.wave.height}px;
-        opacity: ${this.options.wave.opacity};
+        width: ${this.options.animation.flatness * 500}px;
+        height: ${this.options.animation.height}px;
+        opacity: ${this.options.animation.opacity};
         transition: all 100ms linear;
-        transform: translateY(${this.options.wave.height}px);
+        transform: translateY(${this.options.animation.height}px);
       }
       .wave-move{
         position: absolute;
@@ -146,7 +164,7 @@
         width: 100%;
         top: -6%;
         left: 0;
-        animation: move ${10.1 - this.options.wave.speed / 10}s linear infinite;
+        animation: move ${10.1 - this.options.animation.speed / 10}s linear infinite;
       }
       .wave-move::before{
         content: "";
@@ -155,7 +173,7 @@
         height: 100%;
         top:0;
         left: 0;
-        background: url(${this.options.wave.url}) no-repeat center;
+        background: url(${this.options.animation.url}) no-repeat center;
         background-size: 100% 100%;
       }
       .wave-move::after{
@@ -166,8 +184,44 @@
         background: rgba(0, 255, 0, 0.2);
         top:0;
         left: 50%;
-        background: url(${this.options.wave.url}) no-repeat center;
+        background: url(${this.options.animation.url}) no-repeat center;
         background-size: 100% 100%;
+      }
+      .process-bar{
+        position: absolute;
+        width: ${this.options.process.width};
+        height: ${this.options.process.height};
+        left: ${this.options.process.left};
+        bottom: ${this.options.process.bottom};
+        transform: translate(-50%, -50%);
+        z-index: 3;
+      }
+      .process-bar .bar-active{
+        position: relative;
+        height: 100%;
+        background: linear-gradient(to right, ${this.options.process.activeColor1}, ${this.options.process.activeColor2});
+        border-radius: ${parseInt(this.options.process.height)/2}px;
+        overflow: hidden;
+      }
+      .bar-active .bar-cover{
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
+        background: radial-gradient(circle at left, transparent 7px, ${this.options.process.bgColor} 0);
+        transition: all 100ms linear;
+        transform: translateX(0);
+        border-radius: ${parseInt(this.options.process.height)/2}px;
+      }
+      .process-bar .bar-value{
+        position: absolute;
+        top: 50%;
+        right: 0;
+        transform: translate(130%, -50%);
+        color: ${this.options.process.color};
+        font-size: ${this.options.process.fontSize};
+        font-family: MicrosoftYaHei;
       }
       @keyframes move {
         0%{
@@ -177,30 +231,82 @@
           transform: translateX(-50%);
         }
       }
+      @-webkit-keyframes move {
+        0%{
+          transform: translateX(0);
+        }
+        100%{
+          transform: translateX(-50%);
+        }
+      }
+      
+      @keyframes rotate {
+        0% {
+          transform: rotate(0deg);
+        }
+        100% {
+          transform: rotate(360deg);
+        }
+      }
+      @-webkit-keyframes rotate {
+        0% {
+          transform: rotate(0deg);
+        }
+        100% {
+          transform: rotate(360deg);
+        }
+      }
     `;
     },
     domHtml() {
+      let animationDom = `
+        <div class="wave-container" id="wave-container">
+          <div class="wave-move"></div>
+        </div>
+      `;
+      let processDom1 = "";
+      let processDom2 = "";
+      if(this.options.animation.type === "ring"){
+        animationDom = `
+          <div class="ring-box"></div>
+        `
+      }
+      if(this.options.process.type === "bar"){
+        processDom2 = `
+          <div class="process-bar">
+            <div class="bar-active">
+              <div class="bar-cover" id="bar-cover"></div>
+            </div>
+            <div class="bar-value bar-color">
+              <span class="process-value" id="process-value">0</span>%
+            </div>
+          </div>
+        `;
+      }else if(!this.options.logo.url){
+        processDom1 = `
+          <div class="process-box bar-color">
+            <span class="process-value" id="process-value">0</span>
+            <span class="percent-label">%</span>
+          </div>
+        `;
+      }
       return `
       <div class="loading-container" id="loading-container">
         <div class="loading-bg-container">
           <div style="display:none;">
-            <img src="${this.options.wave.url}" importance="high" />
+            <img src="${this.options.animation.url}" importance="high" />
             <img src="${this.options.container.url}" importance="high" />
-            <img src="${this.options.logo && this.options.logo.url}" importance="high" />
             <img src="${this.options.bgUrl}" importance="high" />
+            <img src="${this.options.logo && this.options.logo.url}" importance="high" />
           </div>
         </div>
         <div class="loading-box">
-          <div class="wave-container" id="wave-container">
-            <div class="wave-move"></div>
-          </div>
+          ${animationDom}
           <div class="ball-center" id="ball-center">
-            <div class="process-box">
-              <span class="process-value" id="process-value">0</span>
-              <span class="percent-label">%</span>
-            </div>
+            ${processDom1}
           </div>
         </div>
+        ${processDom2}
       </div>
     `;
     },
@@ -222,11 +328,13 @@
     },
     calculateProcess(maxNum) {
       if(!this.processDom) this.processDom = document.querySelector("#process-value");
+      if(!this.barCover) this.barCover = document.querySelector("#bar-cover");
       if(!this.waveContainer) this.waveContainer = document.querySelector("#wave-container");
       this.process = Math.max(0, (this.sourceNum - 1)) * 10 + this.timerProcess;
       this.process = Math.min(this.process, 100);
-      this.waveContainer && (this.waveContainer.style.transform = `translateY(${this.options.wave.height / 100 * (100-this.process)}px)`);
+      this.waveContainer && (this.waveContainer.style.transform = `translateY(${this.options.animation.height / 100 * (100-this.process)}px)`);
       this.processDom && (this.processDom.innerHTML = this.process);
+      this.barCover && (this.barCover.style.transform = `translateX(${this.process}%)`);
       if (this.timerProcess >= maxNum) {
         window._clearInterval(this.timer);
       }
